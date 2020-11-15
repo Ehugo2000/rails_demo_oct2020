86b3024e8b5fc7bfdeec20d1c736dfc5d4496bce
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-15 22:21:32 +0100

    Setting up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   root controller: :articles, action: :index
   resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 end
